0f2d4769cd8f66824da5f53b9ae1222f916e90ba
ibrarsoomro <50750853+ibrarsoomro@users.noreply.github.com>
2020-09-09 09:53:39 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Components/Navbar';
+import InfoPanel from './Components/InfoPanel';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <InfoPanel />
+    </div>
+  );
+}  
+
+export default App;
